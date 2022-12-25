c554bde5b3e098a4121f11fe90d56cd0f40c9fb4
Pep Mascaro Monserrat <pep.mascaro.monserrat@ba.com>
2022-12-25 02:08:57 +0000

    feat: adding input box to app

--- app.py
+++ app.py
@@ -16,7 +16,7 @@
 # key libraries
 #---
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 from requests.auth import HTTPBasicAuth
 import json # json it will come in handy when parsing the JSON output of an API
@@ -100,7 +100,7 @@ for postcode in list_airports_postcode:
 # # requesting a location - user input
 # origin=get_direction("N1 1SY")
 
-# # focusing in getting information from one origin to LHR - TODO: loop through all airports
+# # focusing in getting information from one origin to LHR 
 # destination_latlon = list_postcode_latlon[0] # LHR only
 
 # my_query = {
@@ -140,8 +140,8 @@ for postcode in list_airports_postcode:
 
 app = Flask(__name__) # 
 
-@app.route("/")
-def test():
+@app.route("/", methods=['GET', 'POST'])
+def home():
     ####
     # Accessing HERE API
     ####
@@ -154,50 +154,80 @@ def test():
     my_url = "https://transit.router.hereapi.com/v8/routes"
 
     # HERE token
-    token = "eyJhbGciOiJSUzUxMiIsImN0eSI6IkpXVCIsImlzcyI6IkhFUkUiLCJhaWQiOiJlMWVaQXRheTZaZFdBdU9vZmt1RSIsImlhdCI6MTY3MTgzODQ1MywiZXhwIjoxNjcxOTI0ODUzLCJraWQiOiJqMSJ9.ZXlKaGJHY2lPaUprYVhJaUxDSmxibU1pT2lKQk1qVTJRMEpETFVoVE5URXlJbjAuLk54WEJPU1lOcGdnRWZCZ09PcFMzZmcuWXVVR1BCSkhiUXFYNjBVcDdhQ1FVREVLM2dhX3dyOGRzTm5TV3kwZnFwMGlTZHFWRjBjT3Y2YWpScGZsTGMweXpRQkVPRllmaXJtSEZudUs4VWJhMXJKbV9lUXhvS2ExRGQxTkU0MXBBTlAxeUFFZUY4Tm1lOWlLV0NEWGtIdU9YQ0JJRHBhNVV6Y1ExQV8yZUVhWDhLUzhvVWpTM0E0ajF5M3FwcFFBb1hFLl9YOTJ1eHJfSk1jUGtUYUtPelNoZGpXSXJXUFF2OW8weXFLRFotOVpMYU0.V7JwYSR_tqJ2iFoQPU31X9QsgA-71-QTjYl0Mq7UOAFPYpvBPoLebIuPEO5zWi5D1w6odS8au6O59DYyLDsgdWvsGxDxZdfw1urMsUjug8f_RK8-EJfBTSP0V3kvdjtPqrr7dYUEvDi3GQKbGnB_HdPs-AScuJMCWGiOPaCTt1CIs59SDp5IgQdjppAl34a7KdI6hbrFEcpRmWmAENu-PjdDRua3FLEz0reK1wkEZWUX7GvFA8vn8e4B2spkBdyWtBHnRYPAhbtBTdyOFJ1ErREaGxfoI0buaw7AgdUXJ-UI3vCcOrWluLnXLbmgHo38MSNKquqXOk91F2CntNdhwQ"
-
+    token = "eyJhbGciOiJSUzUxMiIsImN0eSI6IkpXVCIsImlzcyI6IkhFUkUiLCJhaWQiOiJlMWVaQXRheTZaZFdBdU9vZmt1RSIsImlhdCI6MTY3MTkzMjk1OCwiZXhwIjoxNjcyMDE5MzU4LCJraWQiOiJqMSJ9.ZXlKaGJHY2lPaUprYVhJaUxDSmxibU1pT2lKQk1qVTJRMEpETFVoVE5URXlJbjAuLkdfYjhidG12WWYxMXNSTDQ4cFB2eFEuQ083QWh1T0ZGdDJrMjk0WDJvZDVKSWtoVWt0akJMaVg0N1V3MUtXWnBva29icTExY1NSbWpDcVp4TEJTazh3Y1VuM0VIbkRmTkxBckRiVGRPOHJrNnpOYThVSGE1c2UxX0dsblItUjE5d1FaQ2lyNk1rZzFPZEJCSTJZVXRKdVlMVTJYLVR1RDVuTThMV2I4REswZEw0cVpYaHZHUkk2Y0dwLXlkbzk4azlzLm9PNHZLaVZ4czNDbkpmZU1oU0Z1NHFXRHpCZDJaYVNVM2czSmhFWGhqaE0.ev9z_H0JvwVSwwXu-W7IGMxMnS3QDuLzleIzNRi0EbOB5tOf0FPKeTxp8OA72JFYfOGL2ffRx9CJ5UZOxUJnZ1AFRm2D0338BDK8hVgIK3bmxA4U-ILnzMLhABAQnxmUF8QQu-f4mjZumuR_DUYC-KA83MwsewiczxjR8X97JYcHRJuQIy2Yp-9SSCo-H3wesU256AmGpaca5uz0YSO2kmWPpmBpHxAFrWFHAOdFfVAOQiBBgHZsQmeNTsAGohemBwUuqqE-5AEhP3ZKeHXQ0SAR_O3M1CwKpmcbn45JE0zWBQXZRFAiMKQMPCCQ0ZnHz3srjCjv9rvCMxXYvCMD2Q"
     # headers parameter
     my_headers = {'Authorization' : 'Bearer '+token }
 
-    # requesting a location - user input. 
-    # TODO use flask app (text preprocessing):https://realpython.com/flask-by-example-part-3-text-processing-with-requests-beautifulsoup-nltk/ 
-    # and https://www.youtube.com/watch?v=9MHYHgh4jYc to get user input
-    origin=get_direction("N1 1SY")
+    # errors = [] # initialise empty list to be filled with possible errors from user's input
+    # results = {} # initialise empty dictionary
 
     # initialise an empty list where we will save each of the information texts
     list_text_info = []
 
-    # loop through each destination
-    for destination_latlon, airport_cd in zip(list_postcode_latlon, list_airports_code):
+    if request.method == "POST":
+        # get postcode that the user has entered
+        print(request.form['postcode'])
+        print( type(request.form['postcode']) )
+        # try: # TODO - deal with errors in postcode!
+            # postcode = request.form['postcode']
+            # r = requests.get(postcode)
+            # print(r.text)
+        # except:
+            # errors.append("Unable to get postcode. Please make sure it's valid and try again.")
+            # return render_template('index.html', errors= errors)
+
+        # if r: # if we get a valid postcode
+
+        #----
+        #
+        # Get info for that specific postcode
+        #
+        #----
+
+        # setting origin postcode - origin comes from user input box
+        origin_postcode = request.form['postcode']
+        origin_latlon=get_direction(origin_postcode)
+        
+        print(f"origin: {origin_latlon}")
+
+        # loop through each destination
+        for destination_latlon, airport_cd in zip(list_postcode_latlon, list_airports_code):
+        
+
+            my_query = {
+                            'origin':origin_latlon,
+                            'destination':destination_latlon
+                        }
+
+            print(my_query)            
+            # request a response
+            try:
+                response = requests.get(url=my_url, headers=my_headers, params=my_query, timeout=6) 
+            except ConnectTimeout:
+                print(f'Request has timed out for {airport_cd} airport')
+                break # terminates current loop and resumes execution at the next loop
+
+            # Use the json module to load CKAN's (Comprehensive Knowledge Archive Network) response into a dictionary.
+            data = response.json()
+            # print(data)
+            # getting journey information and time to destination
+            # dep_time =  data['routes'][0]['sections'][0]['departure']['time']
+            dep_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+            arr_time = data['routes'][0]['sections'][len(data['routes'][0]['sections'])-1]['arrival']['time']
+
+            dep_datetime = datetime.strptime(dep_time, '%Y-%m-%dT%H:%M:%SZ')
+            arr_datetime = datetime.strptime(arr_time, '%Y-%m-%dT%H:%M:%SZ')
+
+            diff = arr_datetime - dep_datetime 
+
+            print( f"Your departure time from origin is: {dep_datetime.time()} and your arrival time to destination to the airport is: {arr_datetime.time()}" ) 
+            print(f"Time to destination: {diff} \n")
+
+            list_text_info.append( f"Your departure time from origin is: {dep_datetime.time()} and your arrival time to {airport_cd} is: {arr_datetime.time()}.{os.linesep} Time to destination: {diff}")
+
+        return render_template('index.html', list_text_info=list_text_info) #
     
-
-        my_query = {
-                        'origin':origin,
-                        'destination':destination_latlon
-                    }
-                
-        # request a response
-        response =requests.get(url=my_url, headers=my_headers, params=my_query) 
-
-        # Use the json module to load CKAN's (Comprehensive Knowledge Archive Network) response into a dictionary.
-        data = response.json()
-
-        # getting journey information and time to destination
-        # dep_time =  data['routes'][0]['sections'][0]['departure']['time']
-        dep_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-        arr_time = data['routes'][0]['sections'][len(data['routes'][0]['sections'])-1]['arrival']['time']
-
-        dep_datetime = datetime.strptime(dep_time, '%Y-%m-%dT%H:%M:%SZ')
-        arr_datetime = datetime.strptime(arr_time, '%Y-%m-%dT%H:%M:%SZ')
-
-        diff = arr_datetime - dep_datetime 
-
-        print( f"Your departure time from origin is: {dep_datetime.time()} and your arrival time to destination to the airport is: {arr_datetime.time()}" ) 
-        print(f"Time to destination: {diff} \n")
-
-        list_text_info.append( f"Your departure time from origin is: {dep_datetime.time()} and your arrival time to {airport_cd} is: {arr_datetime.time()}.{os.linesep} Time to destination: {diff}")
-
-    return render_template('index.html', list_text_info = list_text_info)
+    return render_template('index.html', list_text_info=list_text_info) #
 
 #----
 #
